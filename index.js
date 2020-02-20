d181be8ab6c8c699fb045344aa5489895cdfe91f
unknown <@Cungara17>
2020-02-20 18:52:12 -0500

    Setup basic server

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // code away!
+const express = require('express');
+const postRoutes = require('./posts/postRouter');
+const server = express();
+
+
+server.use(express.json())
+
+server.get('/', (req, res)=>{
+    res.json({
+        message: "Welcome to my API :)"
+    })
+})
+
+server.use('/api/posts/', postRoutes);
+
+
+server.listen(4001, ()=>{
+
+    console.log(
+        `%c
+     --------------------------------------------------------------
+     |       ___                                                   |
+     |      (^o^) <Server is running on http://localhost:4001      |
+     |     ((___))                                                 |
+     |       ^ ^                                                   |
+     --------------------------------------------------------------
+            `,
+        "font-family:monospace"
+      );
+
+})
